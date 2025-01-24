f8bcc0e89829e253e9fccc68587f08be30bd6108
JessicaWitzlerI <vwitzler@gmail.com>
2025-01-24 10:10:43 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ['Pizza', 'Sushi']
 
 # função para o nome do programa
 def nome_programa():
@@ -15,28 +15,44 @@ def opcoes():
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
-
 # função para finalizar o app e limpar o terminal
 def finalizar_app():
+    exibir_subtitulo('App finalizado')
+
+# função para voltar ao menu principal 
+def voltar_ao_menu():
+    input('\nDigite uma tecla para voltar ao menu principal. ')
+    main()
+
+# função para exibir os subtitulos
+def exibir_subtitulo(texto):
     os.system('cls')
-    print('App finalizado\n')
+    print(texto)
+    print()
 
 # função para opções inválidas
 def opcao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal. ')
-    main()
+    voltar_ao_menu()
 
-#função para cadastrar os restaurantes
+# função para cadastrar os restaurantes
 def cadastrar_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes\n')
+    exibir_subtitulo('Cadastro de novos restaurantes')
     nome_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     # append adiciona um objeto a lista
-    restaurantes.append
+    restaurantes.append(nome_restaurante)
     print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso!')
-    input('\nDigite uma tecla para voltar ao menu principal. ')
-    main()
+    voltar_ao_menu()
+
+# função para listar os restaurantes
+def listar_restaurantes():
+    exibir_subtitulo('Lista de restaurantes')
+    
+    # para cada restaurante na lista dos restaurantes, ele vai passar o nome do restaurante
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu()
 
 # função para a condicional 
 def escolha_opcao():
@@ -47,7 +63,7 @@ def escolha_opcao():
         if opcao_escolhida == 1:
             cadastrar_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
         elif opcao_escolhida == 4: 
